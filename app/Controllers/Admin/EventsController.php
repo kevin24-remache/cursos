3213c351f4213d77a56e0eac8b20db616710c9a0
19MAX <maxisebas19@gmail.com>
2024-09-02 17:02:39 -0500

    Validaciones de fechas agregados al controlador

--- app/Controllers/Admin/EventsController.php
+++ app/Controllers/Admin/EventsController.php
@@ -6,15 +6,21 @@ use App\Models\CategoryModel;
 use App\Models\EventCategoryModel;
 use App\Models\EventsModel;
 use ModulosAdmin;
+use Modalidad;
 
 class EventsController extends BaseController
 {
-    private function redirectView($validation = null, $flashMessages = null, $last_data = null)
+    private function redirectView($validation = null, $flashMessages = null, $last_data = null, $route = null, $id = null)
     {
-        return redirect()->to('admin/event/new')->
+        return redirect()->to('admin/event/' . $route . '/' . $id)->
             with('flashValidation', isset($validation) ? $validation->getErrors() : null)->
             with('flashMessages', $flashMessages)->
             with('last_data', $last_data);
+    }
+
+    public function __construct()
+    {
+        helper('image');
     }
 
     public function index()
@@ -26,7 +32,7 @@ class EventsController extends BaseController
         $last_action = session()->getFlashdata('last_action');
 
         $eventModel = new EventsModel();
-        $all_events = $eventModel->findAll();
+        $all_events = $eventModel->getAllEventsWithCategories();
 
         $modulo = ModulosAdmin::EVENTS_LIST;
         $data = [
@@ -37,8 +43,10 @@ class EventsController extends BaseController
             'flashMessages' => $flashMessages,
             'modulo' => $modulo,
         ];
-        return view('admin/event', $data);
+
+        return view('admin/events/event', $data);
     }
+
     public function add()
     {
         $event_name = $this->request->getPost('event_name');
@@ -46,6 +54,7 @@ class EventsController extends BaseController
         $event_date = $this->request->getPost('event_date');
         $address = $this->request->getPost('address');
         $modality = $this->request->getPost('modality');
+        $event_duration = $this->request->getPost('event_duration');
         $registrations_start_date = $this->request->getPost('registrations_start_date');
         $registrations_end_date = $this->request->getPost('registrations_end_date');
         $image = $this->request->getFile('image');
@@ -57,11 +66,14 @@ class EventsController extends BaseController
             'event_date' => $event_date,
             'address' => trim($address),
             'modality' => $modality,
+            'event_duration' => $event_duration,
             'registrations_start_date' => $registrations_start_date,
             'registrations_end_date' => $registrations_end_date,
             'categories' => $categories,
         ];
 
+        // Iniciar la transacción
+        $db = \Config\Database::connect();
         try {
             $validation = \Config\Services::validation();
             $validation->setRules(
@@ -71,12 +83,12 @@ class EventsController extends BaseController
                         'rules' => 'required|min_length[3]|is_unique[events.event_name]',
                     ],
                     'short_description' => [
-                        'label' => 'Descripción corta',
-                        'rules' => 'min_length[5]|permit_empty',
+                        'label' => 'Descripción del evento',
+                        'rules' => 'required|min_length[5]',
                     ],
                     'event_date' => [
                         'label' => 'Fecha del evento',
-                        'rules' => 'required|valid_date',
+                        'rules' => 'required|valid_date|fecha_evento',
                     ],
                     'address' => [
                         'label' => 'Dirección',
@@ -84,15 +96,23 @@ class EventsController extends BaseController
                     ],
                     'modality' => [
                         'label' => 'Modalidad',
-                        'rules' => 'required',
+                        'rules' => 'required|in_list[' . implode(',', [
+                            Modalidad::Presencial,
+                            Modalidad::Virtual,
+                            Modalidad::Hibrida,
+                        ]) . ']',
+                    ],
+                    'event_duration' => [
+                        'label' => 'Duración del evento',
+                        'rules' => 'validarDuracion',
                     ],
                     'registrations_start_date' => [
                         'label' => 'Fecha de inicio de inscripciones',
-                        'rules' => 'required|valid_date',
+                        'rules' => 'required|valid_date|fecha_inicio_inscripcion[registrations_end_date,event_date]',
                     ],
                     'registrations_end_date' => [
                         'label' => 'Fecha de fin de inscripciones',
-                        'rules' => 'required|valid_date',
+                        'rules' => 'required|valid_date|fecha_fin_inscripcion[registrations_start_date,event_date]',
                     ],
                     'image' => [
                         'label' => 'Imagen',
@@ -109,8 +129,6 @@ class EventsController extends BaseController
             );
 
             if ($validation->run($data)) {
-                // Iniciar la transacción
-                $db = \Config\Database::connect();
                 $db->transStart();
 
                 unset($data['categories']);
@@ -119,45 +137,40 @@ class EventsController extends BaseController
                 $new_event = $eventsModel->insert($data);
 
                 if ($new_event) {
-                    // Si la inserción fue exitosa, procesar la imagen y las categorías
-                    if ($image->isValid() && !$image->hasMoved()) {
-                        $newName = $image->getRandomName();
-                        $ruta = ROOTPATH . 'public/assets/images/events/';
-                        $image->move($ruta, $newName);
-                        $data['image'] = 'assets/images/events/' . $newName;
+                    $image = $this->request->getFile('image');
+                    $imagePath = uploadImage($image);
 
-                        // Actualizar la entrada en la tabla events con la ruta de la imagen
+                    if ($imagePath !== false) {
+                        $data['image'] = $imagePath;
                         $eventsModel->update($new_event, ['image' => $data['image']]);
+
+                        // Guardar categorías
+                        $eventCategoryModel = new EventCategoryModel();
+                        foreach ($categories as $category_id) {
+                            $eventCategoryModel->insert([
+                                'event_id' => $new_event,
+                                'cat_id' => $category_id,
+                            ]);
+                        }
+
+                        $db->transComplete();
+
+                        return $this->redirectView(null, [['Evento agregado exitosamente', 'success']], null, 'new');
                     } else {
-                        // Si hubo un error con la imagen, revertir la transacción
                         $db->transRollback();
-                        return $this->redirectView($validation, [['Error en los datos enviados', 'warning']], $data);
+                        return $this->redirectView(null, [['Error en la subida de la imagen', 'danger']], $data, 'new');
                     }
-                    // Guardar las categorías en la tabla de relación
-                    $eventCategoryModel = new EventCategoryModel();
-                    foreach ($categories as $category_id) {
-                        $eventCategoryModel->insert([
-                            'event_id' => $new_event,
-                            'cat_id' => $category_id,
-                        ]);
-                    }
-
-                    // Si todo fue bien, confirmar la transacción
-                    $db->transComplete();
-
-                    return $this->redirectView(null, [['Evento agregado exitosamente', 'success']], null);
                 } else {
-                    // Si hubo un error al insertar en la tabla events, revertir la transacción
                     $db->transRollback();
-                    return $this->redirectView(null, [['No fue posible guardar el evento', 'warning']], $data);
+                    return $this->redirectView(null, [['No fue posible guardar el evento', 'warning']], $data, 'new');
                 }
             } else {
-                return $this->redirectView($validation, [['Error en los datos enviados', 'warning']], $data);
+                return $this->redirectView($validation, [['Error en los datos enviados', 'warning']], $data, 'new');
             }
         } catch (\Exception $e) {
             // Si ocurre alguna excepción, revertir la transacción
             $db->transRollback();
-            return $this->redirectView(null, [['No se pudo registrar el evento', 'danger']]);
+            return $this->redirectView(null, [['No se pudo registrar el evento ' . $e->getMessage(), 'danger']], null, 'new');
         }
     }
 
@@ -181,6 +194,180 @@ class EventsController extends BaseController
             'flashMessages' => $flashMessages,
             'modulo' => $modulo,
         ];
-        return view('admin/nuevo_evento', $data);
+        return view('admin/events/nuevo_evento', $data);
+    }
+
+    public function update()
+    {
+        $id = $this->request->getPost('id');
+        $event_name = $this->request->getPost('event_name');
+        $short_description = $this->request->getPost('short_description');
+        $event_date = $this->request->getPost('event_date');
+        $address = $this->request->getPost('address');
+        $modality = $this->request->getPost('modality');
+        $event_duration = $this->request->getPost('event_duration');
+        $registrations_start_date = $this->request->getPost('registrations_start_date');
+        $registrations_end_date = $this->request->getPost('registrations_end_date');
+        $image = $this->request->getFile('image');
+        $categories = $this->request->getPost('id_cat');
+        $status = $this->request->getPost('event_status');
+
+        $data = [
+            'event_name' => trim($event_name),
+            'short_description' => trim($short_description),
+            'event_date' => $event_date,
+            'address' => trim($address),
+            'modality' => $modality,
+            'event_duration' => $event_duration,
+            'registrations_start_date' => $registrations_start_date,
+            'registrations_end_date' => $registrations_end_date,
+            'categories' => $categories,
+            'event_status' => $status
+        ];
+
+        try {
+            $validation = \Config\Services::validation();
+            $validation->setRules(
+                [
+                    'event_name' => [
+                        'label' => 'Nombre del evento',
+                        'rules' => "required|min_length[3]|is_unique[events.event_name,id,{$id}]",
+                    ],
+                    'short_description' => [
+                        'label' => 'Descripción corta',
+                        'rules' => 'min_length[5]|permit_empty',
+                    ],
+                    'event_date' => [
+                        'label' => 'Fecha del evento',
+                        'rules' => 'required|valid_date',
+                    ],
+                    'address' => [
+                        'label' => 'Dirección',
+                        'rules' => 'required',
+                    ],
+                    'modality' => [
+                        'label' => 'Modalidad',
+                        'rules' => 'required|in_list[' . implode(',', [
+                            Modalidad::Presencial,
+                            Modalidad::Virtual,
+                            Modalidad::Hibrida,
+                        ]) . ']',
+                    ],
+                    'event_duration' => [
+                        'label' => 'Duración del evento',
+                        'rules' => 'validarDuracion',
+                    ],
+                    'registrations_start_date' => [
+                        'label' => 'Fecha de inicio de inscripciones',
+                        'rules' => 'required|valid_date',
+                    ],
+                    'registrations_end_date' => [
+                        'label' => 'Fecha de fin de inscripciones',
+                        'rules' => 'required|valid_date',
+                    ],
+                    'image' => [
+                        'label' => 'Imagen',
+                        'rules' => 'is_image[image]|max_size[image,1024]',
+                    ],
+                    'categories' => [
+                        'label' => '
+                         del evento',
+                        'rules' => 'required',
+                        'errors' => [
+                            'required' => 'Debe seleccionar al menos una categoría.'
+                        ]
+                    ],
+                    'event_status' => [
+                        'label' => 'Estado',
+                        'rules' => 'required',
+                    ],
+                ]
+            );
+
+            if ($validation->run($data)) {
+
+
+                $eventsModel = new EventsModel();
+                $newImagePath = null;
+                $oldImagePath = null;
+                unset($data['categories']);
+
+                // Obtener la imagen actual antes de cualquier cambio
+                $current_event = $eventsModel->find($id);
+                $oldImagePath = $current_event['image'] ?? null;
+
+                // Manejar la nueva imagen usando el helper
+                if ($image->isValid()) {
+                    $newImagePath = uploadImage($image);
+                    if (!$newImagePath) {
+                        return $this->redirectView(null, [['Error subiendo la imagen', 'warning']], null, 'edit', $id);
+                    }
+                }
+
+                // Llamar al método del modelo para actualizar el evento
+                $result = $eventsModel->updateEvent($id, $data, $categories, $newImagePath, $oldImagePath);
+
+                if ($result) {
+                    return $this->redirectView(null, [['Evento actualizado exitosamente', 'success']], null, 'edit', $id);
+                } else {
+                    // Si la actualización falló, la nueva imagen ya se habrá eliminado en updateEvent
+                    return $this->redirectView(null, [['No se pudo actualizar el evento', 'danger']], null, 'edit', $id);
+                }
+            } else {
+                return $this->redirectView($validation, [['Error en los datos enviados', 'warning']], $data, 'edit', $id);
+            }
+        } catch (\Exception $e) {
+            return $this->redirectView(null, [['No se pudo actualizar el evento', 'danger']], 'edit', $id);
+        }
+    }
+
+    public function edit_event($id)
+    {
+        // get flash data
+        $flashValidation = session()->getFlashdata('flashValidation');
+        $flashMessages = session()->getFlashdata('flashMessages');
+        $last_data = session()->getFlashdata('last_data');
+        $last_action = session()->getFlashdata('last_action');
+
+        $categoryModel = new CategoryModel();
+        $all_category = $categoryModel->findAll();
+        $eventModel = new EventsModel();
+        $event = $eventModel->getEventDetailsById($id);
+        $data = [
+            'categories' => $all_category,
+            'event' => $event,
+            'last_action' => $last_action,
+            'last_data' => $last_data,
+            'validation' => $flashValidation,
+            'flashMessages' => $flashMessages,
+        ];
+        return view('admin/events/edit_evento', $data);
+    }
+
+    public function delete()
+    {
+        $id = $this->request->getPost('id');
+
+        // Verificar que el ID no esté vacío
+        if (empty($id)) {
+            return $this->redirectView(null, [['El ID del evento es requerido', 'danger']]);
+        }
+
+        try {
+            $eventsModel = new EventsModel();
+            // Buscar el evento por ID
+            $event = $eventsModel->find($id);
+
+            // Verificar si el evento existe
+            if ($event) {
+                $eventsModel->delete($id);
+
+                return $this->redirectView(null, [['Evento eliminado exitosamente', 'success']]);
+            } else {
+                return $this->redirectView(null, [['Evento no encontrado', 'danger']]);
+            }
+        } catch (\Exception $e) {
+            return $this->redirectView(null, [['No se pudo eliminar el evento', 'danger']]);
+        }
     }
 }
---
+++ app/Validation/EventRules.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Validation;
+
+use Modalidad;
+use DateTime;
+
+class EventRules
+{
+    public function validarDuracion(string $str, string &$error = null, array $data = []): bool
+    {
+        if (isset($data['modality']) && ($data['modality'] == Modalidad::Virtual || $data['modality'] == Modalidad::Hibrida)) {
+            if (empty($str)) {
+                $error = 'La duración del evento es requerida para modalidades Virtual o Hibrida.';
+                return false;
+            }
+
+            if (!is_numeric($str)) {
+                $error = 'La duración del evento debe ser un valor numérico.';
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public function fecha_evento(string $valor, &$error = null): bool
+    {
+        $fechaEvento = new DateTime($valor);
+        $hoy = new DateTime();
+
+        if ($fechaEvento <= $hoy) {
+            $error = 'La fecha del evento debe ser una fecha futura';
+            return false;
+        }
+
+        return true;
+    }
+    public function fecha_inicio_inscripcion($value, string $params, array $data, &$error = null): bool
+    {
+        // Obtenemos la fecha de inicio y la fecha de fin de inscripciones
+        $fechaInicio = new DateTime($value);
+        $fechaFin = isset($data['registrations_end_date']) ? new DateTime($data['registrations_end_date']) : null;
+        $fechaEvento = isset($data['event_date']) ? new DateTime($data['event_date']) : null;
+        $hoy = new DateTime();
+        $hoy = $hoy->format('Y-m-d');
+        $fechaInicio= $fechaInicio->format('Y-m-d');
+        $fechaFin= $fechaFin->format('Y-m-d');
+        $fechaEvento= $fechaEvento->format('Y-m-d');
+        // Validar que la fecha de inicio de inscripción no esté en el pasado
+        if ($fechaInicio < $hoy) {
+            $error = 'La fecha de inicio de la inscripción no puede estar en el pasado.';
+            return false;
+        }
+
+        // Validar que la fecha de inicio de inscripción sea anterior a la fecha de fin de inscripción
+        if ($fechaFin && $fechaInicio >= $fechaFin) {
+            $error = 'La fecha de inicio de la inscripción debe ser anterior a la fecha de fin de inscripción.';
+            return false;
+        }
+
+        if ($fechaEvento && $fechaInicio >= $fechaEvento) {
+            $error = 'La fecha de inicio de la inscripción debe ser menor a la fecha del evento';
+            return false;
+        }
+
+        return true;
+    }
+    public function fecha_fin_inscripcion($value, string $params, array $data, &$error = null): bool
+    {
+        // Obtenemos la fecha de inicio y la fecha de fin de inscripciones
+        $fechaFin = new DateTime($value);
+        $fechaInicio = isset($data['registrations_start_date']) ? new DateTime($data['registrations_start_date']) : null;
+        $fechaEvento = isset($data['event_date']) ? new DateTime($data['event_date']) : null;
+        $hoy = new DateTime();
+        $hoy = $hoy->format('Y-m-d');
+        $fechaInicio= $fechaInicio->format('Y-m-d');
+        $fechaFin= $fechaFin->format('Y-m-d');
+        $fechaEvento= $fechaEvento->format('Y-m-d');
+
+        if ($fechaFin < $hoy) {
+            $error = 'La fecha de finalización de la inscripción no puede ser menor a la fecha actual.';
+            return false;
+        }
+
+        if ($fechaEvento && !($fechaFin <= $fechaEvento)) {
+            $error = 'La fecha de finalización de la inscripción debe ser menor o igual a la fecha del evento';
+            return false;
+        }
+
+        if ($fechaInicio && $fechaFin <= $fechaInicio) {
+            $error = 'La fecha de finalización de la inscripción debe ser mayor a la fecha de inicio de la inscripción.';
+            return false;
+        }
+
+        return true;
+    }
+}
