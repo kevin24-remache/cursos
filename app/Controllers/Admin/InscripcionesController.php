4f975156fc1ca411b4d1d6eb8d7f7e6ec78bb509
19MAX <maxisebas19@gmail.com>
2024-09-16 18:55:56 -0500

    Estructura mejorada con el servicio de aprobar

--- app/Controllers/Admin/InscripcionesController.php
+++ app/Controllers/Admin/InscripcionesController.php
@@ -2,59 +2,18 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
-use App\Models\PaymentMethodsModel;
-use App\Models\PaymentsModel;
 use App\Models\RegistrationsModel;
-use CodeIgniter\I18n\Time;
+use App\Services\PaymentAprobarService;
 use ModulosAdminPagos;
-use Dompdf\Dompdf;
-use Dompdf\Options;
 use PaymentStatus;
 
 class InscripcionesController extends BaseController
 {
+    private $paymentAprobarService;
 
-    private function demoPDF($num_autorizacion)
-    {
-        helper('facture');
-        $paymentsModel = new PaymentsModel();
-        $payment = $paymentsModel->numeroAutorizacion($num_autorizacion);
-        if (!$payment) {
-            return false;
-        }
+    public function __construct(){
 
-        // Generar el PDF
-        $pdfData = generate_pdf($payment);
-        $pdfOutput = $pdfData['output'];
-
-        // Guardar el PDF en una ruta accesible
-        $pdfPath = WRITEPATH . 'uploads/comprobante_recaudacion.pdf';
-        file_put_contents($pdfPath, $pdfOutput);
-
-        // Verificar si se debe enviar el correo electrónico
-        if (empty($payment['send_email'])) {
-            helper('email');
-            // Enviar correo electrónico con el PDF adjunto
-            $to = $payment['email_user'];
-            $subject = 'Comprobante de recaudación';
-            $message = 'Adjunto encontrará su comprobante de recaudación.';
-            $result = send_email_with_pdf_from_path($to, $subject, $message, $pdfPath);
-
-            if ($result === 'success') {
-                // Actualizar el campo send_email en la base de datos
-                $paymentsModel->update($payment['id'], ['send_email' => 1]);
-                // Eliminar el archivo PDF temporal
-                unlink($pdfPath);
-                return 'success';
-            } else {
-                // Ocurrió un error al enviar el correo
-                return $result;
-            }
-        }
-
-        // Eliminar el archivo PDF temporal
-        unlink($pdfPath);
-        return 'success';
+        $this->paymentAprobarService = new PaymentAprobarService();
     }
 
     private function redirectView($validation = null, $flashMessages = null, $last_data = null, $ic = null, $estado = null, $uniqueCode = null)
@@ -76,79 +35,26 @@ class InscripcionesController extends BaseController
 
     public function pago()
     {
-        helper('ramdom');
-        $uniqueCode = generateUniqueNumericCode(50);
 
         $id_usuario = session('id');
         $id_pago = $this->request->getPost('id');
         $cedula = $this->request->getPost('cedula');
         $estado_pago = $this->request->getPost('estado_pago');
-        $precio = $this->request->getPost('precio');
+
         if (!$id_pago) {
             return $this->redirectView(null, [['El id no existe', 'warning']], null, $cedula, $estado_pago);
         }
 
-        $paymentsModel = new PaymentsModel();
-        $payment_methodModel = new PaymentMethodsModel();
-        $local = $payment_methodModel->paymentLocal(2);
-        if (!$local) {
-            return $this->redirectView(null, [['Método de pago desactivado', 'warning']], null, $cedula, $estado_pago);
-        }
+        // Usar el servicio para aprobar el pago
+        $result = $this->paymentAprobarService->approvePayment($id_pago, $id_usuario,'2');
 
-        // Obtener el registro del pago
-        $payment = $paymentsModel->find($id_pago);
-        if (!$payment) {
-            return $this->redirectView(null, [['Pago no encontrado', 'warning']], null, $cedula, $estado_pago);
-        }
-        $cantidad = 1;
-        $fecha_emision = Time::now();
-        $precio_unitario = 0.51;
-        $subtotal = $precio_unitario * $cantidad;
-        $valor_total = $precio_unitario * $cantidad;
-        $sub_total_0 = 0.00; // Si no hay subtotales exentos o a 0%
-        $subtotal_15 = $subtotal;
-        $iva_15 = $subtotal * 0.15;
-        // $total_pago = $subtotal + $iva_15;
-        $pago_final = $precio;
-        $total = $precio_unitario + $iva_15;
-        $datosPago = [
-            "num_autorizacion" => $uniqueCode,
-            "date_time_payment" => $fecha_emision,
-            "payment_status" => 2,
-            "amount_pay" => $pago_final,
-            "precio_unitario" => $precio_unitario,
-            "sub_total" => $subtotal,
-            "sub_total_0" => $sub_total_0,
-            "sub_total_15" => $subtotal_15,
-            "iva" => $iva_15,
-            "valor_total" => $valor_total,
-            "total" => $total,
-            "payment_method_id" => 2,
-        ];
-
-        // Verificar si el campo num_autorizacion ya tiene un valor
-        if (isset($payment['num_autorizacion']) && !empty($payment['num_autorizacion']) && ($payment['payment_status'] !== 2)) {
-            // El campo num_autorizacion ya tiene un valor, usar el existente
-            $uniqueCode = $payment['num_autorizacion'];
-            $paymentsModel->update($id_pago, ["payment_status" => 2, "payment_method_id" => 2]);
-
+        // Verificar el resultado del servicio
+        if ($result['success']) {
+            return $this->redirectView(null, [['Pago aprobado y email enviado correctamente', 'success']], null, $cedula, 'Completado', $result['uniqueCode']);
         } else {
-            // El campo num_autorizacion está vacío, actualizar con el nuevo valor
-            try {
-                $paymentsModel->updatePaymentAndInsertInscripcionPago($id_pago, $datosPago, $id_usuario);
-            } catch (\Exception $e) {
-                return $this->redirectView(null, [['Error en la creación del pdf:' . $e->getMessage(), 'warning']], null, $cedula, $estado_pago);
-            }
+            return $this->redirectView(null, [[$result['message'], 'warning']], null, $cedula, $estado_pago);
         }
-        // Llamar al método demoPDF() para generar y enviar el PDF
-        $result = $this->demoPDF($uniqueCode);
 
-        // Verificar el resultado de demoPDF()
-        if ($result === 'success') {
-            return $this->redirectView(null, [['Email enviado con el PDF correctamente', 'success', $uniqueCode]], null, $cedula, 'Completado', $uniqueCode);
-        } else {
-            return $this->redirectView(null, [['Error al enviando el email ', 'error']], null, $cedula, $estado_pago);
-        }
     }
 
     public function index($cedula = null, $estado = null)
