3091fe0b11b39aa0cb298c28dd4414ca48f75a80
19MAX <maxisebas19@gmail.com>
2024-09-16 18:53:21 -0500

    Migracion actualizada

--- app/Database/Migrations/2024-06-07-143103_CreateTablePaymentMethod.php
+++ app/Database/Migrations/2024-06-07-143103_CreateTablePaymentMethod.php
@@ -34,6 +34,23 @@ class CreateTablePaymentMethod extends Migration
         $this->forge->addKey('id', true);
         $this->forge->createTable('payment_methods');
 
+        $data = [
+            [
+                'method_name' => 'Deposito',
+                'description' => 'Pago por deposito'
+            ],
+            [
+                'method_name' => 'Pago físico',
+                'description' => 'Pago en puntos de pago'
+            ],
+            [
+                'method_name' => 'Pago en linea',
+                'description' => 'Pago en el sistema de inscripción'
+            ]
+        ];
+        $this->db->table('payment_methods')->insertBatch($data);
+
+
         // Habilitar chequeo de claves foráneas después de la creación de la tabla
         $this->db->enableForeignKeyChecks();
     }
